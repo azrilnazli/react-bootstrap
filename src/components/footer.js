7cc6ffcf3cb3fe767e7702fce57dcda3a61170ff
Azril Nazli Alias <azril.nazli@gmail.com>
2022-02-13 18:44:34 +0800

    initialize

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return ( 
+        <p>Cover template for <a href="https://getbootstrap.com/" className="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" className="text-white">@mdo</a>.</p>
+    );
+}
+ 
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import Content from './components/content';
+import Header from './components/header';
+import Footer from './components/footer';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<Header />,document.getElementById('header'));
+ReactDOM.render(<Footer />,document.getElementById('footer'));
+ReactDOM.render(<Content />,document.getElementById('content'));
